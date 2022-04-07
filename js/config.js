167cd70fc6220ab3db4482b972e6c780eebb2e32
Logius Standaardenbeheer <78218356+logius-standaardenbeheer@users.noreply.github.com>
2022-04-07 15:23:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -174,7 +174,7 @@ var respecConfig = {
       publisher: "Nederlands Normalisatie-instituut",
     },
     "Digikoppeling Architectuur": {
-      href: "https://centrumvoorstandaarden.github.io/Architectuur2.0-metRestfulAPI/static.html",
+      href: "https://publicatie.centrumvoorstandaarden.nl/dk/architectuur/",
       title:
         "Digikoppeling Architectuur",
       authors: ["Logius Centrum voor standaarden"],
@@ -192,7 +192,6 @@ var respecConfig = {
     "Digikoppeling Beveiligingsdocument": {
       href: "https://publicatie.centrumvoorstandaarden.nl/dk/beveilig/",
       title: "Digikoppeling Beveiligingsstandaarden en voorschriften",
-      date: "2021",
       publisher: "Logius",
     },
     "Digikoppeling-Cert": {
